4d28e4b352fd196d25c6c6efbcbb7f75d3ddb8fc
Vinícius Alves Borba <viniboal28@gmail.com>
2023-12-28 12:29:48 -0300

    first commit

---
+++ src/components/Module.tsx
@@ -0,0 +1,37 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
+
+import { ChevronDown } from "lucide-react";
+import { Lesson } from "./Lesson";
+
+interface ModuleProps {
+  moduleIndex: number;
+  title: string;
+  lessonsAmount: number;
+}
+
+export function Module({ title, lessonsAmount, moduleIndex }: ModuleProps) {
+  return (
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+          {moduleIndex + 1}
+        </div>
+
+        <div className="flex flex-col gap-1 text-left">
+          <strong className="text-xs">{title}</strong>
+          <span className="text-xs text-zinc-400">{lessonsAmount} aulas</span>
+        </div>
+
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Te amo messi" duration="infinito" />
+          <Lesson title="Te amo messi" duration="infinito" />
+          <Lesson title="Te amo messi" duration="infinito" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  );
+}
---
+++ src/pages/Player.tsx
@@ -0,0 +1,34 @@
+import { MessageCircle } from 'lucide-react'
+import { Header } from '../components/Header'
+import { VideoPlayer } from '../components/VideoPlayer'
+import { Module } from '../components/Module'
+
+export function Player() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <div className="flex w-[1100px] flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <Header />
+
+          <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+            Deixar feedback
+            <MessageCircle className="w-4 h-4"/>
+          </button>
+        </div>
+
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
+          <div className="flex-1">
+            <VideoPlayer />
+          </div>
+          
+          <aside className="w-80 absolute top-0 bottom-0 right-0 divide-y-2 divide-zinc-900 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+            <Module moduleIndex={0} title="Messi GOAT" lessonsAmount={10}/>
+            <Module moduleIndex={1} title="Messi GOAT" lessonsAmount={10}/>
+            <Module moduleIndex={2} title="Messi GOAT" lessonsAmount={10}/>
+          </aside>
+        </main>
+      
+      </div>
+    </div>
+  )
+}
