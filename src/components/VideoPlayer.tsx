f697a3c8805f2d197882c9960fba69179b5a89b3
Vinícius Alves Borba <viniboal28@gmail.com>
2023-12-29 20:11:08 -0300

    feat: add autoplayer

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,13 +1,32 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { next } from '../store/slices/player';
 
 export function VideoPlayer() {
+  const dispatch = useDispatch()
+
+  const lesson = useAppSelector((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state.player
+
+    const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+
+    return currentLesson
+  })
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
+        playing
         controls
-        url="https://www.youtube.com/watch?v=twOMe6o1eLU"
+        onEnded={handlePlayNext}
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   );
