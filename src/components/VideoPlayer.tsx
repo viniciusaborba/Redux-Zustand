8868ac91478296502657fcae89f752ff7e8809ed
Vinícius Alves Borba <viniboal28@gmail.com>
2023-12-30 19:09:13 -0300

    feat: add loading animation

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,11 +1,12 @@
 import ReactPlayer from "react-player";
 import { next, useCurrentLesson } from "../store/slices/player";
-import { useAppDispatch } from "../store";
+import { useAppDispatch, useAppSelector } from "../store";
+import { Loader } from "lucide-react";
 
 export function VideoPlayer() {
   const dispatch = useAppDispatch();
-
   const { currentLesson } = useCurrentLesson();
+  const isCourseLoading = useAppSelector(state => state.player.isLoading);
 
   function handlePlayNext() {
     dispatch(next());
@@ -17,14 +18,20 @@ export function VideoPlayer() {
 
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      <ReactPlayer
-        width="100%"
-        height="100%"
-        playing
-        controls
-        onEnded={handlePlayNext}
-        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
-      />
+      {isCourseLoading ? (
+        <div className="flex h-full items-center justify-center">
+          <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
+        </div>
+      ) : (
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          playing
+          controls
+          onEnded={handlePlayNext}
+          url={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
+        />
+      )}
     </div>
   );
 }
